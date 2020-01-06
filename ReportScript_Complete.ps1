07ca8378009e4f3481e17320d7b7070846138a10
smxu10 <57024673+smxu10@users.noreply.github.com>
2020-01-05 23:03:08 -0600

    Add files via upload

---
+++ ReportScript_Complete.ps1
@@ -0,0 +1,35 @@
+﻿# Name of bucket.
+$BucketName = "globomantics-system-reports"
+
+# Variable to store output data.
+$ReportData = @()
+
+# Test S3 Bucket exists.
+if (Test-S3Bucket -BucketName $BucketName) {
+    # Get contents of bucket.
+    $BucketContents = Get-S3Object -BucketName $BucketName
+
+    # Loop through contents.
+    foreach ($Object in $BucketContents) {
+        # Download each file.
+        $LocalFile = Read-S3Object `
+            -BucketName $BucketName `
+            -Key $Object.Key `
+            -File .\local-file.dat
+
+        # Read contents.
+        $Contents = $LocalFile | Import-Csv -Delimiter " " -Header IPAddress
+
+        # Record top 5 IP Addresses from each file.
+        $ReportData += $Contents `
+            | Group-Object -Property IPAddress `
+            | Sort-Object -Property Count -Descending `
+            | Select-Object -First 5 -Property Count, Name
+
+        # Delete local file.
+        $LocalFile | Remove-Item
+    }
+
+    # Write results to output file.
+    $ReportData | ConvertTo-Csv | Out-File .\report.csv -Force
+}
---
+++ ReportScript_Incomplete.ps1
@@ -0,0 +1,32 @@
+﻿# Name of bucket.
+$BucketName = "globomantics-system-reports"
+
+# Variable to store output data.
+$ReportData = @()
+
+# Test S3 Bucket exists.
+if ("TODO: TEST IF BUCKET EXISTS") {
+    # Get contents of bucket.
+    $BucketContents = "TODO: GET BUCKET CONTENTS"
+
+    # Loop through contents.
+    foreach ($Object in $BucketContents) {
+        # Download each file.
+        $LocalFile = "TODO: DOWNLOAD FILE"
+
+        # Read contents.
+        $Contents = $LocalFile | Import-Csv -Delimiter " " -Header IPAddress
+
+        # Record top 5 IP Addresses from each file.
+        $ReportData += $Contents `
+            | Group-Object -Property IPAddress `
+            | Sort-Object -Property Count -Descending `
+            | Select-Object -First 5 -Property Count, Name
+
+        # Delete local file.
+        $LocalFile | Remove-Item
+    }
+
+    # Write results to output file.
+    $ReportData | ConvertTo-Csv | Out-File .\report.csv -Force
+}
