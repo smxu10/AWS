aa77913795ac49b31a3801011c6f12eca756d9a2
smxu10 <57024673+smxu10@users.noreply.github.com>
2020-01-05 22:42:55 -0600

    Add files via upload

---
+++ CFN_CLI_Powershell.ps1
@@ -0,0 +1,15 @@
+﻿aws cloudformation describe-stack-events --stack-name check-output
+aws cloudformation delete-stack --stack-name check-output 
+
+aws cloudformation create-stack --stack-name check-output `
+--template-body file://C://Minzhe/AWS/Udemy_Courses/AWS_CloudFormation_Introduction/code/6-outputs/0-create-ssh-security-group.yaml
+
+aws cloudformation describe-stacks --stack-name check-output
+
+aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE
+aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE | select-string StackName
+
+aws cloudformation list-stacks --stack-status-filter DELETE_COMPLETE
+aws cloudformation list-stacks --stack-status-filter DELETE_COMPLETE | select-string StackName
+aws cloudformation list-stacks --stack-status-filter DELETE_COMPLETE | select-string StackName | Where-Object {$_ -Like  "*lambda*"}
+
